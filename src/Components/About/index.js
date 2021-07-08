9a3c8146de13bb77ff5403cf2ba0196c41218f28
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-07-08 17:04:23 -0400

    consult on bootstrap

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Profile2 from '../../assets/me/Profile2.png'
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function About() {
   return (
     <section>
-      <h1 id="about">About Me</h1>
-      <p className="about">My name is Marilyn and this portoflio showcases what I have learned as a web developer student.  I will receive my certificate from the University of Toronto Bootcamp program in August of this year.  Until recently, I was working as a flight attendant.  This was a very rewarding career and the perks were amazing!   
-    
-    I received my bachelors degree from the University of Windsor.  While there, I attended some HTML and CSS classes.  My education also focoused on logic and argument. I thoroughly enjoyed those types of concepts and decided to work that into a new career.  Always intrigued by the world of a web developer I decided to take the plunge!</p>
+       <h2 id="about">About Me</h2>
+      <Container>
+        <Row>
+          <Col xs={6} md={4}>
+            <Image src= {Profile2} width="180" height="171" alt="profile picture" roundedCircle />
+          </Col>
+          <Col xs={12} md={8}>
+          <p>My name is Marilyn and this portoflio showcases what I have learned as a web developer student.  I will receive my certificate from the University of Toronto Bootcamp program in August of this year.  Until recently, I was working as a flight attendant.  This was a very rewarding career and the perks were amazing!   
+          
+          I received my bachelors degree from the University of Windsor.  While there, I attended some HTML and CSS classes.  My education also focoused on logic and argument. I thoroughly enjoyed those types of concepts and decided to work that into a new career.  Always intrigued by the world of a web developer I decided to take the plunge!</p>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 }
--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -9,17 +9,17 @@ function Navigation({setCurrent, components}) {
 
   return (
     <header>
-      <Navbar bg="light" expand="lg">
+      <Navbar className="Navbar" expand="lg">
         <Container>
-        <Navbar.Brand href="#/">Marilyn Papadopoulos</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link onClick={()=>setCurrent(components[0])}>About Me</Nav.Link>
-            <Nav.Link onClick={()=>setCurrent(components[1])}>Portfolio</Nav.Link>
-            <Nav.Link onClick={()=>setCurrent(components[2])}>Contact</Nav.Link>
-            <Nav.Link href="#https://docs.google.com/document/d/1ePBWSn9vXs_U7HjXYHE0TXyLnwt2x8qbRlxRhNVxLnk/edit?usp=sharing">Resume</Nav.Link> 
-          </Nav>
+          <Navbar.Brand href="#/" id="name">Marilyn Papadopoulos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto" >
+              <Nav.Link className="Nav-link" onClick={()=>setCurrent(components[0])}>About Me</Nav.Link>
+              <Nav.Link className="Nav-link" onClick={()=>setCurrent(components[1])}>Portfolio</Nav.Link>
+              <Nav.Link className="Nav-link" onClick={()=>setCurrent(components[2])}>Contact</Nav.Link>
+              <Nav.Link className="Nav-link" href="#https://docs.google.com/document/d/1ePBWSn9vXs_U7HjXYHE0TXyLnwt2x8qbRlxRhNVxLnk/edit?usp=sharing">Resume</Nav.Link> 
+            </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
