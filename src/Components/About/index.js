d15b5785c963ca91d42a297481c2990fc2470cbc
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-09-17 11:02:10 -0400

    changed about text

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -12,9 +12,12 @@ function About() {
             <Image src= {Profile2} width="180" height="171" alt="profile picture" roundedCircle />
           </Col>
           <Col xs={12} md={8}>
-          <p className="about-text">My name is Marilyn and this portoflio showcases what I have learned as a web developer student.  I will receive my certificate from the University of Toronto Bootcamp program in August of this year.  Until recently, I was working as a flight attendant.  This was a very rewarding career and the perks were amazing!   
-          
-          I received my bachelors degree from the University of Windsor.  While there, I attended some HTML and CSS classes.  My education also focused on logic and argument. I thoroughly enjoyed those types of concepts and decided to work that into a new career.  Always intrigued by the world of a web developer I decided to take the plunge!</p>
+          <p className="about-text">I am a front-end developer maximizing my background in aviation and law to be a part of a more valuable web user experience. 
+          I recently earned a certificate in full stack development from the University of Toronto.  While there I developed skills in HTML, CSS, Javascript and React.js.  
+          I naturally enjoy problem solving and find it exciting to work through solutions using previous experience, research or brainstorming with my peers.  
+          I am passionate about learning, and with each project my goal is to further develop my skill set to enhance the user experience.  I worked on an agile team of five to develop a single-page MERN social app for dogs.
+          I am excited to leverage my skills as part of quality-driven team and build better experiences on the web.
+</p>
           </Col>
         </Row>
       </Container>
